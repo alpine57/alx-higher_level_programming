8b05b2472a3df2abb6183508c728c30da2a2bbec
alpine57 <kamutileeongmail.com>
2024-01-13 03:05:00 -0800

    python data structures

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    list_count = len(my_list)
+    if idx < 0 or idx >= list_count:
+        return None
+    else:
+        return my_list[idx]
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    list_count = len(my_list)
+    if idx < 0 or idx >= list_count:
+        return my_list
+    else:
+        new_list = my_list.copy()
+        new_list[idx] = element
+    return new_list
